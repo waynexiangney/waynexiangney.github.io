2afffee2a2c46ca0535128ae08ae955c48a2836f
unknown <shengxia@usc.edu>
2021-10-12 23:23:46 -0700

    format and update resume

--- css/hw8.py
+++ css/hw8.py
@@ -1,20 +1,22 @@
-
 #!/usr/bin/python
 
-import sys
 import math
+import sys
 
 Data = []
 X = []
+
+
 def ReadCSV():
     FileName = sys.argv[1]
     f = open(FileName)
     for line in f:
         Data.append(line)
-    f.close() 
-    for line in Data:    
-        f = line.strip().split(',')
+    f.close()
+    for line in Data:
+        f = line.strip().split(",")
         X.append(f)
     return X
+
 
 X = ReadCSV()
